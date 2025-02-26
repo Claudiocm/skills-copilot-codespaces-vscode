73f5228daf18bacb5a97c35658af1770bdf11206
Claudio C. Matos <claudiocm_7@hotmail.com>
2025-02-26 23:42:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,14 @@
+function skillsMember() {
+    var skills = {
+        "name": "Member",
+        "skills": [
+            "HTML",
+            "CSS",
+            "JavaScript",
+            "React",
+            "Node.js"
+        ]
+    };
+    return skills;
+}
+module.exports = skillsMember;
